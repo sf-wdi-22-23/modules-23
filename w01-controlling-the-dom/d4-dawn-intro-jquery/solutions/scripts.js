76177fd1fc76263b183626fb6865d8baccd17325
Juliana <jlopker@gmail.com>
2015-09-30 17:02:45 -0700

    added solutions for todo app

---
+++ w01-controlling-the-dom/d4-dawn-intro-jquery/solutions/scripts.js
@@ -0,0 +1,49 @@
+$(function() {
+
+  // `toDos` array is our model (holds our data)
+  // contains test (or "seed") data
+  var toDos = [
+    {name: "laundry", desc: "clean clothes"},
+    {name: "grocery shopping", desc: "buy food"},
+    {name: "nap time", desc: "remember to sleep!"}
+  ];
+
+  // form to create new todo
+  var $newToDo = $('#new-todo');
+  
+  // element to hold our list of todos
+  var $toDoList = $('#todo-list');
+
+  // append existing todos (from seed data) to `$toDoList`
+  for (var i=0; i< toDos.length; i++) {
+    $toDoList.append('<li class="todo">' + toDos[i].name + ' - ' + toDos[i].desc + '</li>');
+  }
+
+  // submit form to create new todo
+  $newToDo.on('submit', function(event) {
+    event.preventDefault();
+
+    // create new todo object from form data
+    var toDoName = $('#todo-name').val();
+    var toDoDesc = $('#todo-desc').val();
+    var toDoData = {name: toDoName, desc: toDoDesc};
+
+    // store our new todo
+    toDos.push(toDoData);
+
+    console.log(toDos);
+
+    // append our new todo to the page
+    $toDoList.append('<li class="todo">' + toDoData.name + ' - ' + toDoData.desc + '</li>');
+
+    // reset the form
+    $newToDo[0].reset();
+    $('#todo-name').focus();
+  });
+
+  // add class to todo on click to mark it as done
+  $toDoList.on('click', '.todo', function() {
+    $(this).addClass('done');
+  });
+
+});
