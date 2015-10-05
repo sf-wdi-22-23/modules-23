f9526aea3fdd8761cf9059920236d5291e8662a2
Ben Hulan <benhulan@gmail.com>
2015-10-05 01:11:11 -0700

    dusk functions lesson and exercises complete

---
+++ w02-working-with-objects/d1-dusk-functions/exercises.md
@@ -0,0 +1,55 @@
+####Looping Exercises (Optional, for extra practice):
+Begin with the JSON object from the lecture:
+
+```
+var myFirstJSON = 
+	{"language basics":
+		[{"Spanish":
+			{
+                                    "letters":["a", "b", "c","ch","d","e","f","g","h","i","j","k","l","ll","m","n","&ntilde;","o","p",
+                                    "q","r","rr","s","t","u","v","w","x","y","z"],
+                                    "numbers":["uno","dos","tres","quatro","cinco","seis","siete","ocho","nueve","diez",
+                                    "once","doce","trece","catorce","quince","dieciseis","diecisiete","dieciocho",
+                                    "diecinueve","veinte"]
+			}
+                      },
+		{"Japanese":
+			{
+                                    "letters":["a","i","u","e","o","ka","ki","ku","ke","ko","sa","shi","su","se","so",
+                                    "ta","chi","tsu","te","to","na","ni","nu","ne","no","ha","hi","hu","he","ho","ma",
+                                    "mi","mu","me","mo","ya","yu","yo","ra","ri","ru","re","ro","wa","n"],
+                                    "numbers": ["ichi","ni","san","shi","go","roku","shichi","hachi","kyuu","jyuu",
+                                    "jyuui chi","jyuu ni","jyuu san","jyuu yon","jyuu go","jyuu roku","jyuu nana",
+                                    "jyuu hachi","jyuu kyuu","ni jyuu"]
+			}
+		}]
+	};
+```
+**Exercise 1. Loop through the ```"Spanish" "letters"```.**
+
+**Exercise 2. Write a function languageBasics() that will take a language and a type and return a list stating the first, next and last values of the data requested**
+
+
+####Closure Exercise 1:
+Write a function that takes a number as it’s only argument, let’s name the parameter firstNum and include one function inside. Have the inner function take one number as it’s argument, let’s name that one secondNum. Now in the inner function, add firstNum and secondNum. See closure recipe for step-by-step guidance!
+
+
+####Closure Exercise 2:
+**Step 1. Create an Animal object
+**Step 2. Give the Animal a name attribute and a string value.
+**Step 3. Give the Animal a 'speak' method and a string value.
+**Step 4. Instantiate several Animals and call their speak methods, returning different values for each.
+
+
+####Closure Exercise 3:
+Imagine we are coding a demolition derby-style driving game. In the following exercises, we will create a Car object and give it a variety of attributes, including functions, then we will make callbacks to those internal functions.
+
+**Step 1. Create a new Object and assign it to the variable 'Car'
+**Step 1. Give 'Car' a 'brand' attribute and assign it a string
+**Step 1. Give 'Car' a 'year' attribute and assign it a Number (or a Date() if you know how!)
+**Step 1. Give 'Car' a 'horsepower' attribute and assign it a Number, such as 350
+**Step 1. Add anything else you think would be cool to track in your game. You might enjoy classic game attributes such as speed, hit points, armor, shields, ammunition... or maybe you'd prefer to do something a little more peaceful.
+**Step 1. Now write two functions to change the values of a car object.
+**Step 1. Finally, create two instances of the Car object and call their internal functions (methods).
+**Step 1. Stretch. Can you pass in an argument for one to act on the other and let them battle it out in the console?
+
--- w02-working-with-objects/d1-dusk-functions/solutions.js
+++ w02-working-with-objects/d1-dusk-functions/solutions.js
@@ -1,39 +1,108 @@
+// Looping Exercise 1 Solution:
+for (var key in myFirstJSON){
+    for (var i = 0; i < myFirstJSON['language basics'][0]['Spanish']['letters'].length; i++){
+        if (i === 0){
+            console.log('la primera letra es ' + myFirstJSON['language basics'][0]['Spanish']['letters'][i] );
+        } else {
+            console.log('el proximo letra es ' + myFirstJSON['language basics'][0]['Spanish']['letters'][i] );
+        }
+    }
+}
 
+// Looping Exercise 2 Solution:
 function languageBasics(language, type){
     var tempArrayRoot = myFirstJSON['language basics'];
-        if(language === 'Spanish'){
-    	for (var i = 0; i < tempArrayRoot[0][language][type].length; i++){
-    		if (type === 'letters'){
-                            if (i === 0){
-    			console.log('la primera letra es ' + tempArrayRoot[0][language][type][i] );
-               		} else {
-    			console.log('el proximo letra es ' + tempArrayRoot[0][language][type][i] );
-    		       }
-                    } else if (type === 'numbers') {
-                            if (i === 0){
-                                console.log('la primera numero es ' + tempArrayRoot[0][language][type][i] );
-                            } else {
-                                console.log('el proximo numero es ' + tempArrayRoot[0][language][type][i] );
-                            }
-                      }
+    if(language === 'Spanish'){
+       for (var i = 0; i < tempArrayRoot[0][language][type].length; i++){
+		if (type === 'letters'){
+                        if (i === 0){
+			console.log('la primera letra es ' + tempArrayRoot[0][language][type][i] );
+           	       } else {
+			console.log('el proximo letra es ' + tempArrayRoot[0][language][type][i] );
+		       }
+                } else if (type === 'numbers') {
+                        if (i === 0){
+                           console.log('la primera numero es ' + tempArrayRoot[0][language][type][i] );
+                        } else {
+                           console.log('el proximo numero es ' + tempArrayRoot[0][language][type][i] );
+                        }
                 }
-            } else if (language === 'Japanese'){
-                for (var j = 0; j < tempArrayRoot[1][language][type].length; j++){
-                    if (type === 'letters') {
-                             if (j === 0){
-                            console.log('hajimete no ji ha ' + tempArrayRoot[1][language][type][j] );
-                            } else {
-                                console.log('tsugi no ji ha ' + tempArrayRoot[1][language][type][j] );
-                            } 
-                        } else if (type === 'numbers'){
-                            if (j === 0){
-                            console.log('hajimete no ban ha ' + tempArrayRoot[1][language][type][j]);
-                            } else {
-                                console.log('tsugi no ban ha ' + tempArrayRoot[1][language][type][j]);
-                            } 
-                    }
+            }
+       } else if (language === 'Japanese'){
+            for (var j = 0; j < tempArrayRoot[1][language][type].length; j++){
+                if (type === 'letters') {
+                         if (j === 0){
+                           console.log('hajimete no ji ha ' + tempArrayRoot[1][language][type][j] );
+                        } else {
+                           console.log('tsugi no ji ha ' + tempArrayRoot[1][language][type][j] );
+                        } 
+                } else if (type === 'numbers'){
+                        if (j === 0){
+                           console.log('hajimete no ban ha ' + tempArrayRoot[1][language][type][j]);
+                        } else {
+                           console.log('tsugi no ban ha ' + tempArrayRoot[1][language][type][j]);
+                        } 
                 }
-            } else {
-                 console.log("Please enter either 'Spanish' or 'Japanese' and either 'letters' or 'numbers'");
-        }
+            }
+       } else {
+             console.log("Please enter either 'Spanish' or 'Japanese' and either 'letters' or 'numbers'");
+       }
 }
+
+// Stretch:  Refactor the function above. There are many ways. Consider using forEach, map, reduce or similar jQuery methods
+
+
+
+// Closure Exercise 1:
+var closeCall = function(firstNum){
+    var secondNum = 6;
+    var addStuff = function(secondNum){
+        return firstNum + secondNum;
+    };
+    return addStuff(secondNum);
+};
+
+// Closure Exercise 2:
+var Animal = function(){
+    return {
+        'name': 'beaver',
+        'age': 2,
+        'gender': 'male',
+        'catchPhrase': 'Dam!',
+        speak: function(words){
+            console.log("hi there, " + words); 
+        },
+        doSomething: function(placeholder){
+            console.log(placeholder + " I just cut down a tree!");
+        }
+    };
+};
+
+var chucky = new Animal();
+
+chucky.speak('I am a beaver!');
+
+
+// Closure Exercise 3:
+
+var Car = {};
+
+Car.brand = "Toyota";
+Car.model = "Corolla";
+Car.year = 1984;
+Car.horsePower = 250;
+Car.hitPoints = 10;
+Car.shields = 5;
+Car.maxSpeed = 50;
+Car.weapons = [{"machine gun": 80}, {"oil slick": 30}, {"wheel swords": 55}];
+Car.attack = function(){
+    var weapons = this.weapons;
+    for(var i = 0; i < weapons.length; i++){
+        for (var key in weapons[i]){
+            console.log( key + ' attack! Damage done: ' + weapons[i][key] );
+        }
+    }
+}
+
+
+
