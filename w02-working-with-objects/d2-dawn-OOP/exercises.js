ad428ea49f1dc60d9f23bf6320becb19bd4991ca
Alex White <awhit012@gmail.com>
2015-10-06 09:43:22 -0700

    quick edit

---
+++ w02-working-with-objects/d2-dawn-OOP/exercises.js
@@ -0,0 +1,17 @@
+###Quick Challenge
+- Copy the object above and save it to a javaScript file, `data.js`
+- Add some properties that logically fit into an object describing our class (address, floor number, and a list of instructors.
+- Run your file using 'node data.js' in your console.
+- Add some lines to 'data.js' to console.log some of your new properties!
+
+If everything worked out, you should have a fully functioning data object, only now with even MORE properties with us to play with!
+
+
+###Quick Challenge - She loves me, she loves me not...
+Create an object method for flower that will play the age old game ['He loves me, he loves me not...'](https://en.wikipedia.org/wiki/He_loves_me..._he_loves_me_not)
+- Count down from the petal number down to 1
+- Alternately display 'He loves me' or 'He loves me not' to the console for each petal count decrement.
+- Display the final phrase with an exclamation; that's the end of the game!
+
+
+There are many more aspects to objects that we will discover tomorrow.  For now, play with objects and think up some great object examples that we might use in class.
