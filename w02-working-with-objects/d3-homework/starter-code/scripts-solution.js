279a1f5d9ace457e5a02857c958602af9bd10fcd
Ben Hulan <benhulan@gmail.com>
2015-10-06 23:29:10 -0700

    adding styles array

--- w02-working-with-objects/d3-homework/starter-code/scripts-solution.js
+++ w02-working-with-objects/d3-homework/starter-code/scripts-solution.js
@@ -10,7 +10,8 @@ $(document).ready(function() {
         directionsDisplay = new google.maps.DirectionsRenderer();
         map = new google.maps.Map(document.getElementById('map-canvas'), {
             center: pos,
-            zoom: 13
+            zoom: 13,
+            styles: [{"featureType":"administrative","stylers":[{"visibility":"off"}]},{"featureType":"poi","stylers":[{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"water","stylers":[{"visibility":"simplified"}]},{"featureType":"transit","stylers":[{"visibility":"simplified"}]},{"featureType":"landscape","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"visibility":"off"}]},{"featureType":"road.local","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"water","stylers":[{"color":"#abbaa4"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"color":"#3f518c"}]},{"featureType":"road.highway","stylers":[{"color":"#ad9b8d"}]}]
         });
     }
 
