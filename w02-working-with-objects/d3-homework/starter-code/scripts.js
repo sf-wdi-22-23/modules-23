0ef06f7256b77850ee32beff71cce04657b186ce
Ben Hulan <benhulan@gmail.com>
2015-10-07 17:54:12 -0700

    removing homework

--- w02-working-with-objects/d3-homework/starter-code/scripts.js
+++
@@ -1,7 +0,0 @@
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 37.790841, lng: -122.401280},  // General Assembly, SF
-    zoom: 8
-  });
-}
